cb1d88d016058c9136ee43b184fb920536527e2f
M.Cooper Healy <m.cooper.healy@gmail.com>
2023-08-17 13:27:16 -0500

    Reorder for companies rather than Universities

--- auto/resume.el
+++ auto/resume.el
@@ -5,13 +5,6 @@
                      '(("article" "a4paper" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("xcolor" "usenames" "dvipsnames") ("layaureo" "big") ("textpos" "absolute")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -35,8 +28,6 @@
     "fontspec")
    (TeX-add-symbols
     '("lmline" 1)
-    "br")
-   (LaTeX-add-xcolor-definecolors
-    "linkcolour"))
+    "br"))
  :latex)
 
