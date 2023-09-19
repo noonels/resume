dce0d5ad0f419b63fe372dc2d6200dea17c04adf
M.Cooper Healy <m.cooper.healy@gmail.com>
2023-09-19 11:09:35 -0500

    Add MS to resume

--- auto/resume.el
+++ auto/resume.el
@@ -5,6 +5,13 @@
                      '(("article" "a4paper" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("xcolor" "usenames" "dvipsnames") ("layaureo" "big") ("textpos" "absolute")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -28,6 +35,8 @@
     "fontspec")
    (TeX-add-symbols
     '("lmline" 1)
-    "br"))
+    "br")
+   (LaTeX-add-xcolor-definecolors
+    "linkcolour"))
  :latex)
 
